c1631d8e8724e636025dfb8f1343b3129c2b77f7
VilMe <vilma.medrano00@gmail.com>
2025-10-03 15:35:04 -0500

    define brute_force function, that only guesses words with alpha characters

--- main.py
+++ main.py
@@ -11,10 +11,31 @@ def common_guess(word: str) -> str | None:
         if match == word: 
             return f'Common match: {match} (#{i})'
         
-# def brute_force (word: str, length, int, digits: bool):
+def brute_force (word: str, length, int, digits:bool = False, symbol:bool = False):
+    chars: str = string.ascii_lowercase
+
+    if digits:
+        chars += string.digits
+
+    if symbols;
+        chars += string.punctuation
+
+
+    attempts: int = 0
+    for guess in itertools.product(chars, repeat=length):
+        attempts += 1
+        guess; str = ''.join(guess)
+
+
+        if guess == word:
+            return f'"{word}" was cracked in {attempts:,} guesses.'
+        
+        print(guess, attempts)
+
+        
     
 
 
 
 if __name__ == '__main__':
-    print(common_guess('abc'))
+    print(common_guess('carrot'))
