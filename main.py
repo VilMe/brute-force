10917235628acc959770d6ceef46e3579d4160da
VilMe <vilma.medrano00@gmail.com>
2025-10-02 21:56:43 -0500

    define common_guess function to open and ready text file with common words

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import itertools
 import string
 import time
 
+def common_guesss(word: str) -> str | None:
+    with open('words.txt', 'r') as words:
+        word_list: list[str] = words.read().splitlines()
+
+
+    
