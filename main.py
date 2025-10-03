2c855a69e6baa05d9d4df5f9c904407551c44e99
VilMe <vilma.medrano00@gmail.com>
2025-10-03 15:42:34 -0500

    define main function with checking guesses

--- main.py
+++ main.py
@@ -32,10 +32,26 @@ def brute_force (word: str, length, int, digits:bool = False, symbol:bool = Fals
         
         print(guess, attempts)
 
-        
+
+def main():
+    print('Searching...')
+    password: str = 'abc1' 
+
+    start_time: float = time.perf_counter()
+
+
+    if common_match := common_guess(password):
+        print(common_match)
+    else:
+        if cracked := brute_force(password, length=4, digits=True, symbols=True):
+            print(cracked)
+        else: 
+            print('There was no match...)
     
+    end_time:float = time.perf_counter()
+    print(round(end_time - start_time, 2), 's')
 
 
 
 if __name__ == '__main__':
-    print(common_guess('carrot'))
+    main()
